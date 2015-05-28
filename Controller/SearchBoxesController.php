1e4c610d371a72e35519655b8b0f416271cc1b98
topaz2 <topaz2.3333@gmail.com>
2015-05-28 17:34:32 +0900

    Fix phpdoc error

--- Controller/SearchBoxesController.php
+++ Controller/SearchBoxesController.php
@@ -1,6 +1,4 @@
 <?php
-App::uses('AppController', 'Controller');
-App::uses('Topic', 'Topics.Model');
 /**
  * SearchBoxes Controller
  *
@@ -10,6 +8,13 @@ App::uses('Topic', 'Topics.Model');
  * @author Jun Nishikawa <topaz2@m0n0m0n0.com>
  * @link http://www.netcommons.org NetCommons Project
  * @license http://www.netcommons.org/license.txt NetCommons License
+ */
+
+App::uses('AppController', 'Controller');
+App::uses('Topic', 'Topics.Model');
+
+/**
+ * Summary for SearchBoxes Controller
  */
 class SearchBoxesController extends AppController {
 
